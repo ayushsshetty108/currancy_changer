d38359f8f1b9aae02aa34060c089b8b0d40b3f8a
ayushsshetty108 <ayushsshetty108@gmail.com>
2026-04-30 01:00:41 +0530

    convertor

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASEURL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
-let dropdown=document.querySelectorAll(".select_container select");
-let btn=document.querySelector("form button");
+let dropdown = document.querySelectorAll(".select_container select");
+let btn = document.querySelector("form button");
+let fromcurr = document.querySelector(".from .select1 select");
+let tocurr = document.querySelector(".to .select1 select");
+let msg=document.querySelector(".msg");
+for (let select of dropdown) {
+  for (let cur in countryList) {
+    let newoption = document.createElement("option");
+    newoption.innerText = cur;
+    newoption.value = cur;
 
-for(let select of dropdown){
-   for(let cur in countryList){
-        let newoption=document.createElement("option");
-        newoption.innerText=cur;
-        newoption.value=cur;
-        if(select.name==="from" && cur==="USD"){
-              newoption.selected="selected"
-        }else if(select.name==="to" && cur==="INR"){
-             newoption.selected="selected"
-        }
-        select.append(newoption);
-   }
-select.addEventListener("change",(evt)=>{
-       UpdateList(evt.target);
-})
+    if (select.name === "from" && cur === "USD") {
+      newoption.selected = true;
+    } else if (select.name === "to" && cur === "INR") {
+      newoption.selected = true;
+    }
+
+    select.append(newoption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    UpdateList(evt.target);
+  });
 }
-const UpdateList=(element)=>{
-    let current=element.value;
-    let countrycode=countryList[current];
-    let imgsor = `https://flagsapi.com/${countrycode}/flat/64.png`;
-     let img=element.parentElement.querySelector("img");
-    img.src=imgsor;
+
+const UpdateList = (element) => {
+  let current = element.value;
+  let countrycode = countryList[current];
+  let imgsor = `https://flagsapi.com/${countrycode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = imgsor;
+};
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();{
+  let amt = document.querySelector(".amount input");
+  let amtval = amt.value;
+  if (amtval === "" || amtval < 0) {
+    amtval = 1;
+    amt.value = "1";
+  }
+
+  console.log(fromcurr.value, tocurr.value);
+  const URL = `${BASEURL}/${fromcurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  console.log(response);
+
+  let rate = data[fromcurr.value.toLowerCase()][tocurr.value.toLowerCase()];
+  console.log(rate);
+  let finalamt=amtval*rate;
+  console.log(finalamt);
+ 
+  msg.innerText=`${amtval} ${fromcurr.value}=${finalamt} ${tocurr.value}`;
 }
-btn.addEventListener("click",()=>{
-    
-})
+});
